0ae4fbbad4e25a51480bb771fadaecc5e937bdc5
pk401440 <pk401440@ULIB-LOAN-1690>
2024-11-02 12:24:17 -0700

    Add project files.

---
+++ DataModel/Account.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataModel
+{
+    [Table("ACCOUNTS")]
+    public partial class Account
+    {
+        [Key]
+        [Column("ACCOUNT_ID")]
+        public int AccountId { get; set; }
+
+        [Column("ACCOUNT_NUMBER")]
+        public int AccountNumber { get; set; }
+
+        [Column("USER_ID")]
+        public int UserId { get; set; }
+
+        [ForeignKey("UserId")]
+        [InverseProperty("Accounts")]
+        public virtual User User { get; set; } = null!;
+
+
+    }
+}
---
+++ DataModel/BankContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataModel
+{
+    public partial class BankContext : DbContext
+    {
+        public BankContext() { }
+
+        public BankContext(DbContextOptions<BankContext> options) : base(options) { }
+
+        public virtual DbSet<User> Users { get; set; }
+        public virtual DbSet<Account> Accounts { get; set; }
+
+
+    }
+}
