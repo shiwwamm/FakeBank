488d9633a33996b2698290e63bb70b81343ed82c
Shivam Verma <vermashivams1510@gmail.com>
2024-11-03 14:52:23 -0800

    added basic buttons

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { UserComponent } from './components/user/user.component';
+import { AccountsComponent } from './components/accounts/accounts.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+	{
+		path: "users",
+		component: UserComponent
+	},
+	{
+		path: "accounts",
+		component: AccountsComponent
+	}
+];
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { UserComponent } from '../user/user.component';
+import { AccountsComponent } from '../accounts/accounts.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [UserComponent, AccountsComponent, RouterLink],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
