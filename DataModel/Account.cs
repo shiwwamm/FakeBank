67cfdaad5fbaa2f62afa45919c788525fbd022e6
shiwam <vermashivams1510@gmail.com>
2024-11-02 20:34:55 -0700

    Initial setup after scaffold

--- DataModel/Account.cs
+++ DataModel/Account.cs
@@ -1,31 +1,15 @@
-﻿using Microsoft.EntityFrameworkCore;
-using System;
+﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace DataModel
+namespace DataModel;
+
+public partial class Account
 {
-    [Table("ACCOUNTS")]
-    public partial class Account
-    {
-        [Key]
-        [Column("ACCOUNT_ID")]
-        public int AccountId { get; set; }
+    public int AccountId { get; set; }
 
-        [Column("ACCOUNT_NUMBER")]
-        public int AccountNumber { get; set; }
+    public int AccountNumber { get; set; }
 
-        [Column("USER_ID")]
-        public int UserId { get; set; }
+    public int UserId { get; set; }
 
-        [ForeignKey("UserId")]
-        [InverseProperty("Accounts")]
-        public virtual User User { get; set; } = null!;
-
-
-    }
+    public virtual User User { get; set; } = null!;
 }
---
+++ DataModel/BankDbContext.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataModel;
+
+public partial class BankDbContext : DbContext
+{
+    public BankDbContext()
+    {
+    }
+
+    public BankDbContext(DbContextOptions<BankDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Account> Accounts { get; set; }
+
+    public virtual DbSet<User> Users { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=UserAccountDB;Trusted_Connection=True;MultipleActiveResultSets=true");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Account>(entity =>
+        {
+            entity.HasOne(d => d.User).WithMany(p => p.Accounts)
+                .HasForeignKey(d => d.UserId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK_Accounts_Users");
+        });
+
+        modelBuilder.Entity<User>(entity =>
+        {
+            entity.Property(e => e.PhoneNumber).HasMaxLength(15);
+            entity.Property(e => e.Username).HasMaxLength(50);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
