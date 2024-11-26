c76d564e7ee893b571f5349bdfebaafbe952d7eb
shiwwamm <vermashivams1510@gmail.com>
2024-11-25 20:43:37 -0800

    adding the data

---
+++ FakeBank/Data/AllUserAndAccounts.cs
@@ -0,0 +1,27 @@
+﻿namespace FakeBank.Data
+{
+    public class AllUserAndAccounts
+    {
+
+        public string first_name { get; set; } = null!;
+        public string last_name { get; set; } = null!;
+        public string email { get; set; } = null!;
+        public string phone_number { get; set; } = null!;
+        public string account_number { get; set; } = null!;
+        public string routing_number { get; set; } = null!;
+        public string account_status { get; set; } = null!;
+        public string account_type { get; set; } = null!;
+        public double account_balance { get; set; }
+        public string card_number { get; set; } = null!;
+        public string card_expiration { get; set; } = null!;
+        public int card_cvv { get; set; }
+        public string card_status { get; set; } = null!;
+        public DateTime loan_issue_date { get; set; }
+        public int loan_duration { get; set; }
+        public double loan_interest { get; set; }
+        public double loan_amount { get; set; }
+        public string loan_type { get; set; } = null!;
+
+
+    }
+}
